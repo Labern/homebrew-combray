1a1ce5d508f34918f50cddb8ba0412467174b04e
Labern <lukelabern@gmail.com>
2026-06-27 18:33:22 +0100

    combray 0.5

--- Casks/combray.rb
+++ Casks/combray.rb
@@ -1,6 +1,6 @@
 cask "combray" do
-  version "0.4"
-  sha256 "a24d54e52e07e9d0396067723e78e20e38e93a1281f3f4dc897dd37643086ece"
+  version "0.5"
+  sha256 "b20ecf7ab5398a706ed83881def7b035a0f93681baa94dada5f273051c08e5f5"
   url "https://github.com/Labern/Combray/releases/download/v#{version}/Combray.pkg"
   name "Combray"
   desc "Personal archive that transcribes handwritten letters with Claude"
